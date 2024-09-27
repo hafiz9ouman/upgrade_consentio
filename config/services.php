1bb6d0aa432fe27aae357ca20c14470a094b7903
hafiz9ouman <hafiz9oman.dev@gmail.com>
2024-09-27 19:54:53 +0500

    secret/client id fixed

--- config/services.php
+++ config/services.php
@@ -34,6 +34,12 @@ return [
         'secret' => env('SPARKPOST_SECRET'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+    ],
+
     'stripe' => [
         'model' => App\User::class,
         'key' => env('STRIPE_KEY'),
